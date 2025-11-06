1711f782f9eeae6800dc767cdffeee1bcea0347b
Asti22 <uun3461@gmail.com>
2025-11-06 09:23:04 +0700

    membuat fungsi DataApp

---
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -0,0 +1,26 @@
+package com.example.navigasiku
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController
+        )
+    }
+}
