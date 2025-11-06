e7c806cb25e689034a2d889c01b3107b1c0f0b7b
Asti22 <uun3461@gmail.com>
2025-11-06 15:02:08 +0700

    Menambahkan fungsi FormIsian

---
+++ app/src/main/java/com/example/navigasiku/view/FormIsian.kt
@@ -0,0 +1,16 @@
+package com.example.navigasiku.view
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun FormIsian(
+    jenisK : List<String> = listOf("Laki-Laki","Perempuan"),
+    OnSubmitBtnClick : ()->
+){
+    Scaffold (modifier = Modifier,
+        topBar = {
+
+        })
+}
