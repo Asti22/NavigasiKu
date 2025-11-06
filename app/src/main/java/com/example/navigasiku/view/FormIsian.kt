4358e801ea93cb72d8d1b68c07f0a9929ca109cf
Asti22 <uun3461@gmail.com>
2025-11-06 15:12:03 +0700

    Menambahkan Scaffold dengan TopAppBar berwarna teal dan judul dari string resource home di bagian atas layar.

--- app/src/main/java/com/example/navigasiku/view/FormIsian.kt
+++ app/src/main/java/com/example/navigasiku/view/FormIsian.kt
@@ -1,16 +1,34 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigasiku.view
 
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.navigasiku.R
 
 @Composable
 fun FormIsian(
-    jenisK : List<String> = listOf("Laki-Laki","Perempuan"),
-    OnSubmitBtnClick : ()->
-){
-    Scaffold (modifier = Modifier,
+    jenisK: List<String> = listOf("Laki-Laki", "Perempuan"),
+    onSubmitBtnClick: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier,
         topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            ) }
+    ){
 
-        })
-}
+    }
