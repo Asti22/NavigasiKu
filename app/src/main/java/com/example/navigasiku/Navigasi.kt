88b4e7d7a9f0aae33cf806a22aa52e4d8587e9b7
Asti22 <uun3461@gmail.com>
2025-11-06 09:34:53 +0700

    tambah Scaffold dan NavHost untuk navigasi dari Formulir ke Detail

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,11 +1,12 @@
 package com.example.navigasiku
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -16,11 +17,31 @@ enum class Navigasi {
 @Composable
 fun DataApp(
     navController: NavHostController = rememberNavController(),
-    modifier: Modifier
-){
-    Scaffold { isiRuang->
+    modifier: Modifier = Modifier
+) {
+    Scaffold { isiRuang ->
         NavHost(
-            navController = navController
-        )
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                FormIsian(
+                    // contoh: pilihanJK = jenisK.map { id -> context.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilanData(
+                    onBackBtnClick = {cancelAndBackToFormulir(navController) }
+                )
+            }
+        }
     }
 }
+
+
+
+
